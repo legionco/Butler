7d4044685c4e777c6d80bd072bee65692f04a99c
Nick O'Neill <nick.oneill@gmail.com>
2015-12-09 19:09:07 -0800

    Initial Commit

---
+++ Butler/Butler.h
@@ -0,0 +1,19 @@
+//
+//  Butler.h
+//  Butler
+//
+//  Created by Nick O'Neill on 12/9/15.
+//  Copyright © 2015 922am Burrito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Butler.
+FOUNDATION_EXPORT double ButlerVersionNumber;
+
+//! Project version string for Butler.
+FOUNDATION_EXPORT const unsigned char ButlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Butler/PublicHeader.h>
+
+
